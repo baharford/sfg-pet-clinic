0703d76d43c491ded8342b18032c13eb80bda117
m02242a <Brian.Harford@experian.com>
2021-11-08 13:22:01 -0500

    refactored to common crud interface. closes #10

--- pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -1,15 +1,7 @@
 package services;
 
-import java.util.Set;
-
 import guru.springframework.model.Vet;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
-	Vet findById(Long id);
-	
-	Vet save(Vet vet);
-	
-	Set<Vet> findAll();
-	
 }
