dd7a58cc5b62ad2381ff4ccb08d0c124649920f5
m02242a <Brian.Harford@experian.com>
2021-11-30 09:12:23 -0500

    added pets and contact info to owners mapping. closes #33

--- pet-clinic-data/src/main/java/guru/springframework/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/services/map/OwnerServiceMap.java
@@ -5,14 +5,43 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.model.Owner;
+import guru.springframework.model.Pet;
 import guru.springframework.services.OwnerService;
+import guru.springframework.services.PetService;
+import guru.springframework.services.PetTypeService;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
+	private final PetTypeService petTypeService;
+	private final PetService petService;
+	
+	public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+		this.petTypeService = petTypeService;
+		this.petService = petService;
+	}
+
 	@Override
 	public Owner save(Owner object) {
-		return super.save(object);
+		if(object != null) {
+			if(object.getPets() != null) {
+				object.getPets().forEach(pet -> {
+					if(pet.getPetType() != null ) {
+						pet.setPetType(petTypeService.save(pet.getPetType()));
+					}
+					else {
+						throw new RuntimeException("PetType is Required.");
+					}
+					if(pet.getId() == null) {
+						Pet savedPet = petService.save(pet);
+						pet.setId(savedPet.getId());
+					}
+				});
+			}
+			return super.save(object);
+		}
+		else
+			return null;
 	}
 	
 	@Override
--- pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -1,9 +1,12 @@
 package guru.springframework.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springframework.model.Owner;
+import guru.springframework.model.Pet;
 import guru.springframework.model.PetType;
 import guru.springframework.model.Vet;
 import guru.springframework.services.OwnerService;
@@ -37,12 +40,34 @@ public class DataLoader implements CommandLineRunner {
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
+		owner1.setAddress("123 Brickerel");
+		owner1.setCity("Miami");
+		owner1.setTelephone("1231231234");
+		
+		Pet mikesPet = new Pet();
+		mikesPet.setPetType(savedDogPetType);
+		mikesPet.setOwner(owner1);
+		mikesPet.setBirthDate(LocalDate.now());
+		mikesPet.setName("Rosco");
+		
+		owner1.getPets().add(mikesPet);
 		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Glenanne");
+		owner2.setAddress("321 Brickerel");
+		owner2.setCity("Miami");
+		owner2.setTelephone("3213214321");
+
+		Pet fionasPet = new Pet();
+		fionasPet.setPetType(savedCatPetType);
+		fionasPet.setOwner(owner2);
+		fionasPet.setBirthDate(LocalDate.now());
+		fionasPet.setName("Garfield");
+		
+		owner2.getPets().add(fionasPet);
 		
 		ownerService.save(owner2);
 		
