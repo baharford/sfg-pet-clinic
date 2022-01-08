3ad4dd1e782180142a2230f1bc8f930fffb52e46
m02242a <baharford@gmail.com>
2022-01-08 12:34:20 -0500

    add spring data jpa visit service. closes #48

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/springdatajpa/VisitSDPJpaService.java
@@ -0,0 +1,51 @@
+package guru.springframework.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.model.Visit;
+import guru.springframework.repositories.VisitRepository;
+import guru.springframework.services.VisitService;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDPJpaService implements VisitService {
+
+	private final VisitRepository visitRepository;
+	
+	public VisitSDPJpaService(VisitRepository visitRepository) {
+		this.visitRepository = visitRepository;
+	}
+
+	@Override
+	public Set<Visit> findAll() {
+		Set<Visit> visits = new HashSet<>();
+		visitRepository.findAll().forEach(visits::add);
+		
+		return visits;
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return visitRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Visit save(Visit object) {
+		return visitRepository.save(object);
+	}
+
+	@Override
+	public void delete(Visit object) {
+		visitRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		visitRepository.deleteById(id);
+	}
+
+}
